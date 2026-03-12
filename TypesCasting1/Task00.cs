0b360bb01c3b88348ed2134dd6280d13233d748d
Zitian Harper <Zitian.harper@icloud.com>
2026-03-12 11:12:59 +0000

    task00 connected

---
+++ TypesCasting1/Task00.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TypesCasting1
+{
+    internal class Task00
+    {
+       public static void Run()
+        {
+            Console.WriteLine("This works!");
+        }
+    }
+}
