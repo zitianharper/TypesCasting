11bead0cda673ee49e3f9ebe0ee777b1af7a8527
Zitian Harper <Zitian.harper@icloud.com>
2026-03-12 10:51:07 +0000

    connected to github

---
+++ TypesCasting1/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace TypesCasting1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
